456e3400ed1a9451ead5eaab27cda2380f4be4a2
Max <phraine@gmail.com>
2018-05-15 16:19:00 +0200

    Complete example

--- js/main.js
+++ js/main.js
@@ -112,25 +112,22 @@ $(document).ready(function() {
 	}
 
 	function positionLoop() {
+    // Check if a face is detected, and if so, track it.
 		requestAnimationFrame(positionLoop);
-		var position = ctrack.getCurrentPosition();
+		currentPosition = ctrack.getCurrentPosition();
     overlayCC.clearRect(0, 0, vid_width, vid_height);
-    if (position) {
-      currentPosition = position;
-      // console.info(currentPosition);
-      capture();
+    if (currentPosition) {
+      trackFace(currentPosition);
       ctrack.draw(overlay);
-    } else {
-      // requestAnimationFrame(positionLoop);
     }
 	}
 
-  function getFaceRect(position) {
+  function getEyesRect(position) {
+    // Given a tracked face, returns a rectangle surrounding the eyes.
     var minX = position[19][0];
     var maxX = position[15][0];
     var minY = Math.min(position[20][1], position[21][1], position[17][1], position[16][1]);
     var maxY = Math.max(position[23][1], position[26][1], position[31][1], position[28][1]);
-    // var maxY = Math.max(position[26][1]);
 
     var width = maxX - minX;
     var height = maxY - minY;
@@ -138,8 +135,9 @@ $(document).ready(function() {
     return [minX, minY, width, height * 1.5];
   }
 
-  function capture() {
-    var rect = getFaceRect(currentPosition);
+  function trackFace(position) {
+    // Given a tracked face, crops out the eyes and draws them in the eyes canvas.
+    var rect = getEyesRect(position);
 
     var $video = $('#video');
     var tempCanvas = document.getElementById('temp');
@@ -154,4 +152,189 @@ $(document).ready(function() {
 
     eyesCtx.drawImage(tempCanvas, rect[0], rect[1], rect[2], rect[3], 0, 0, eyesCanvas.width, eyesCanvas.height);
   }
+
+
+  /*********** Code for the ball position *********/
+
+	function moveFollowBallRandomly() {
+    // Move the ball to a random position.
+    var x = 0.01 + Math.random() * 0.98;
+    var y = 0.01 + Math.random() * 0.98;
+
+    moveBall(x, y, 'followBall');
+	}
+
+  function moveBall(x, y, id) {
+    // Given relative coordinates, moves the ball there.
+    var left = x * $(document).width() - 40;
+    var top = y * $(document).height() - 40;
+
+    var $ball = $('#' + id);
+    $ball.css('left', left + 'px');
+    $ball.css('top', top + 'px');
+  }
+
+  function getFollowBallPos() {
+    // Get the normalized ball position.
+    var $ball = $('#followBall');
+    var left = $ball.css('left');
+    var top = $ball.css('top');
+    var x = Number(left.substr(0, left.length - 2)) + 20;
+    var y = Number(top.substr(0, top.length - 2)) + 20;
+
+    return [x / $(document).width(), y / $(document).height()];
+  }
+
+  moveFollowBallRandomly();
+
+
+  /*********** Code for collecting a dataset *********/
+
+  // The dataset:
+  var x = null;
+  var y = null;
+  var inputWidth = $('#eyes').width();
+  var inputHeight = $('#eyes').height();
+
+  function getImage() {
+    // Capture the current image in the eyes canvas as a tensor.
+    return tf.tidy(function() {
+      var image = tf.fromPixels(document.getElementById('eyes'));
+      var batchedImage = image.expandDims(0);
+      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    });
+  }
+
+  function addExample(image, target) {
+    // Given an image and target coordinates, adds them to our dataset.
+    target = tf.tidy(function() { return tf.tensor1d(target).expandDims(0); });
+    if (x == null) {
+      x = tf.keep(image);
+      y = tf.keep(target);
+    } else {
+      var oldX = x;
+      x = tf.keep(oldX.concat(image, 0));
+
+      var oldY = y;
+      y = tf.keep(oldY.concat(target, 0));
+
+      oldX.dispose();
+      oldY.dispose();
+      target.dispose();
+    }
+  }
+
+  function captureExample() {
+    // Take the latest image from the eyes canvas and add it to our dataset.
+    // Takes the coordinates of the ball.
+    tf.tidy(function() {
+      var img = getImage();
+      var ballPos = getFollowBallPos();
+      addExample(img, ballPos);
+    });
+  }
+
+
+  /*********** Code for training a model *********/
+
+  var currentModel = null;
+
+  function createModel() {
+    var model = tf.sequential({
+      layers: [
+        tf.layers.conv2d({
+          inputShape: [inputHeight, inputWidth, 3],
+          kernelSize: 3,
+          filters: 16,
+          strides: 1,
+          activation: 'relu',
+          kernelInitializer: 'varianceScaling',
+        }),
+        tf.layers.maxPooling2d({
+          poolSize: [4, 4],
+          strides: [4, 4],
+        }),
+        tf.layers.flatten(),
+        tf.layers.dropout({
+          rate: 0.2,
+        }),
+        tf.layers.dense({
+          units: 2,
+          activation: 'sigmoid',
+          kernelInitializer: 'varianceScaling',
+        }),
+      ]
+    });
+
+    optimizer = tf.train.adam(0.01);
+
+    model.compile({optimizer: optimizer, loss: 'meanSquaredError'});
+
+    return model;
+  }
+
+  function fitModel(model) {
+    var n = x.shape[0];
+
+    var epochs = 5 + Math.floor(n * 0.5);
+
+    var batchSize = Math.floor(n * 0.1);
+    if (batchSize < 4) {
+      batchSize = 4;
+    } else if (batchSize > 32) {
+      batchSize = 32;
+    }
+    console.info('Training on', n, 'samples');
+
+    model.fit(x, y, {
+      batchSize: batchSize,
+      epochs: epochs,
+      shuffle: true,
+      validationSplit: .1,
+      callbacks: {
+        onEpochEnd: function(epoch, logs) {
+          console.info('Epoch', epoch, 'losses:', logs);
+        },
+        onTrainEnd: function() {
+          console.info('Finished training:', model);
+        },
+      }
+    });
+  }
+
+  function moveModelBall() {
+    if (currentModel == null) {
+      return;
+    }
+    tf.tidy(function() {
+      var img = getImage();
+      var prediction = currentModel.predict(img);
+      moveBall(prediction.get(0, 0), prediction.get(0, 1), 'modelBall');
+    });
+  }
+
+  setInterval(moveModelBall, 100);
+
+
+
+  /*********** Code for UI / control *********/
+
+  $('body').keyup(function(e) {
+    // On space key:
+    if (e.keyCode == 32) {
+      captureExample();
+      setTimeout(moveFollowBallRandomly, 100);
+
+      e.preventDefault();
+      return false;
+    }
+
+    // On enter key:
+    if (e.keyCode == 13) {
+      if (currentModel == null) {
+        currentModel = createModel();
+      }
+      fitModel(currentModel);
+    }
+  });
 });
